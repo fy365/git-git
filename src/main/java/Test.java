12eafba5f7b7d80b50834e9cc4599df1cf49e39b
FangYong <915219023@qq.com>
2020-05-18 13:40:11 +0800

    项目666

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -6,7 +6,7 @@
  */
 public class Test {
     public static void main(String[] args) {
-        System.out.println("项目经理创建项目,开发人员加了一句话,开发666");
+        System.out.println("项目经理创建项目,开发人员加了一句话,开发666，项目666");
     }
 
     //开发人员写了一句话
